2cb90712e19b3f69ec38a3b724513459a40c51b9
Alan Zanatta <alan.zanatta@epitech.eu>
2015-06-20 13:10:12 +0200

    Adds component sub generator

---
+++ generators/component/index.js
@@ -0,0 +1,53 @@
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+  // The name `constructor` is important here
+  constructor: function () {
+    // Calling the super constructor is important so our generator is correctly set up
+    generators.Base.apply(this, arguments);
+  },
+  initializing: function() {
+
+  },
+  prompting: function () {
+    var done = this.async();
+    this.prompt([{
+      type    : 'input',
+      name    : 'name',
+      message : 'Your component name',
+    }, {
+      type    : 'input',
+      name    : 'filename',
+      message : 'Your file name',
+      default : function(answers) {
+        return answers.name + '.jsx';
+      }
+    }], function (answers) {
+      this.answers = answers;
+      done();
+    }.bind(this));
+  },
+  configuring: function() {
+
+  },
+  default: function() {
+
+  },
+  writing: function() {
+    // write .gitignore
+    this.fs.copyTpl(this.templatePath("component.jsx"), this.destinationPath("./lib/components/" + this.answers.filename), {
+      component: {
+        name: this.answers.name
+      }
+    });
+  },
+  conflicts: function() {
+
+  },
+  install: function() {
+
+  },
+  end: function() {
+
+  }
+});
---
+++ generators/component/templates/component.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+
+var <%= component.name %> = React.createClass({
+  getInitialState: function() {
+    return {};
+  },
+  componentDidMount: function() {
+
+  },
+  componentWillUnmount: function() {
+
+  },
+  render: function() {
+    return (
+      <div></div>
+    );
+  }
+});
